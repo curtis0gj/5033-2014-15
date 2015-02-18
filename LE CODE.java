fda58429d0f761a71a87eb29cc8ef677bd299809
curtis0gj <curtis.0gj@gmail.com>
2015-02-17 19:10:48 -0500

    Update LE CODE.java

--- LE CODE.java
+++ LE CODE.java
@@ -1,3 +1,4 @@
+
 package org.usfirst.frc.team5033.robot;
 
 
@@ -16,7 +17,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Robot extends SampleRobot {
-    RobotDrive robot;
+	RobotDrive robot;
     Joystick stick;
     Joystick xbox;
     Encoder encoder;	
@@ -30,14 +31,14 @@ public class Robot extends SampleRobot {
     DigitalInput limit2;
     DigitalInput limit3;
     DigitalInput limit4;
-    double Kp = 0.01;
+    double Kp = 0.03;
     boolean bottomscrewlimit = false;
     boolean limitPressed2 = false;
     boolean minarmlimit = false;
     boolean maxarmlimit = false;
-    double x = 0;
-    SendableChooser autoChooser;
-    Defines.AUTOS autoMethod;
+	double x = 0;
+	SendableChooser autoChooser;
+	Defines.AUTOS autoMethod;
 
     public Robot() {
     	robot = new RobotDrive(0, 1);
@@ -63,7 +64,6 @@ public class Robot extends SampleRobot {
         autoChooser.addDefault("Pick Up Two", Defines.AUTOS.AUTO_TWO);
         autoChooser.addObject("Pick Up One", Defines.AUTOS.AUTO_ONE);
         autoChooser.addObject("Move Into Autozone", Defines.AUTOS.AUTO_MOVE);
-        autoChooser.addObject("Do Nothing", Defines.AUTOS.AUTO_NOTHING);
         SmartDashboard.putData("Auto mode", autoChooser);
     }
 
@@ -72,10 +72,56 @@ public class Robot extends SampleRobot {
 
     	if(autoMethod == Defines.AUTOS.AUTO_ONE) 
     		return;
-    	if(autoMethod == Defines.AUTOS.AUTO_NOTHING)
-    		return;
-    	if(autoMethod == Defines.AUTOS.AUTO_MOVE) {
+    		/*double ScrewTime1 = Timer.getFPGATimestamp();
     		while(true) {
+    			if(!isAutonomous() || !isEnabled())
+    				return;
+    			if(ScrewTime1 + 3.5 > Timer.getFPGATimestamp()) {
+    				screwmotor1.set(Defines.SCREW_SPEED);
+    				screwmotor2.set(Defines.SCREW_SPEED);
+    			} else {
+    				screwmotor1.set(Defines.SCREW_OFF);
+    				screwmotor2.set(Defines.SCREW_OFF);
+    				break;	
+    			}
+    		}
+    		screwmotor1.set(Defines.SCREW_OFF);
+        	screwmotor2.set(Defines.SCREW_OFF);
+        	encoder.reset();
+        	gyro.reset();
+        	Timer.delay(Defines.AUTO_TIMEBREAKS); 
+        	
+        	while(true) {
+        		//Turn 90, stop and wait one second.
+        		double angle1 = gyro.getAngle();
+        		if(!isAutonomous() || !isEnabled())
+        			return;
+        		robot.drive(-.40, 1);
+        		if(angle1 < 90) {
+        			break;
+        		}
+        	}
+
+        	robot.drive(0, 0);
+        	encoder.reset();
+        	gyro.reset();
+        	Timer.delay(Defines.AUTO_TIMEBREAKS);
+
+        	while(true) {
+        		//Drive forward until distance is met and stop.
+        		double angle1 = gyro.getAngle();
+        		double distance1 = encoder.get();
+        		if(!isAutonomous() || !isEnabled())
+        			return;
+        		robot.drive(-.25, angle1 * Kp);
+        		if(distance1 < -500) {
+        			robot.drive(0, 0);
+        			return;
+        			//break?
+        		}
+*/
+    	if(autoMethod == Defines.AUTOS.AUTO_MOVE) 
+    		/*while(true) {
     			//Drive to auto zone.
         		double distance = encoder.get();
         		double angle = gyro.getAngle();
@@ -83,111 +129,114 @@ public class Robot extends SampleRobot {
     				return;
     			robot.drive(-0.25, angle * Kp);
 	    		if(distance < -3000) {
-	    			robot.drive(0, 0);
+	    			robot.drive(0, 0);*/
 	    			return;
-	    		}
-    		}
-    	}
-		
-    	double ScrewTime = Timer.getFPGATimestamp();
+	    			//break?
+
+    	double ScrewTime2 = Timer.getFPGATimestamp();
     	while(true)  {
     		//Lift garbage can for 3 seconds and stop, wait one second.
     		if(!isAutonomous() || !isEnabled())
     			return;
-    		if(ScrewTime + 3 > Timer.getFPGATimestamp()) {
+    		if(ScrewTime2 + 3.5 > Timer.getFPGATimestamp()) {
     			screwmotor1.set(Defines.SCREW_SPEED);
     			screwmotor2.set(Defines.SCREW_SPEED);
     		} else {
     			break;
     		}
     	}
-
     	screwmotor1.set(Defines.SCREW_OFF);
     	screwmotor2.set(Defines.SCREW_OFF);
+    	encoder.reset();
+    	gyro.reset();
     	Timer.delay(Defines.AUTO_TIMEBREAKS); 
-
     	while(true) {
     		//Travel to the bin, stop, reset sensors and wait one second.
-    		double angle = gyro.getAngle();
-    		double distance = encoder.get();
+    		double angle2 = gyro.getAngle();
+    		double distance2 = encoder.get();
+    		SmartDashboard.putNumber("distance", distance2);
     		if(!isAutonomous() || !isEnabled())
     			return;
-    		robot.drive(-.25, angle * Kp);
-    		if(distance < -100) {
+    		robot.drive(-.25, angle2 * Kp);
+    		if(distance2 < -450) {
     			break;
     		}
     	}
-
     	robot.drive(0, 0);
     	encoder.reset();
     	gyro.reset();
-    	Timer.delay(Defines.AUTO_TIMEBREAKS); // again, what if auto ends?
-
+    	Timer.delay(Defines.AUTO_TIMEBREAKS);
     	double ArmTime = Timer.getFPGATimestamp();
     	while(true) {
     		//Close arms for two seconds and stop, wait one.
-    		// || && maxarmlimit = limit4.get();
     		if(!isAutonomous() || !isEnabled())
     			return;
     		maxarmlimit = limit4.get();
     		if(ArmTime + 2 > Timer.getFPGATimestamp() && maxarmlimit == true) {
     			armmotor.set(Defines.ARM_SPEED);
+				leftarmwheel.set(Relay.Value.kForward);
+	        	rightarmwheel.set(Relay.Value.kReverse);
     		} else {
     			break;
     		}
     	}
-
-    	armmotor.set(0);
+    	armmotor.set(Defines.ARM_OFF);
+    	leftarmwheel.set(Relay.Value.kOff);
+    	rightarmwheel.set(Relay.Value.kOff);
+    	armmotor.set(Defines.ARM_SPEED);
+    	leftarmwheel.set(Relay.Value.kForward);
+    	rightarmwheel.set(Relay.Value.kReverse);
     	encoder.reset();
+    	gyro.reset();
     	Timer.delay(Defines.AUTO_TIMEBREAKS);
-
-    	while(true) {
-    		//Turn 90, stop and wait one second.
-    		double angle = gyro.getAngle();
+		while(true) {
+    		//Drive forward until distance is met and stop.
+    		double angle3 = gyro.getAngle();
+    		SmartDashboard.putNumber("angle3", angle3);
     		if(!isAutonomous() || !isEnabled())
     			return;
-    		robot.drive(-.40, 1);
-    		if(angle < 90) {
+    		robot.drive(-.40, -1);
+    		if(angle3 > 70) {
     			break;
     		}
-    	}
-
-    	robot.drive(0,0);
+    		robot.drive(0, 0);
+		}
     	encoder.reset();
+    	gyro.reset();
     	Timer.delay(Defines.AUTO_TIMEBREAKS);
-
     	while(true) {
     		//Drive forward until distance is met and stop.
-    		double angle = gyro.getAngle();
-    		double distance = encoder.get();
+    		double angle2 = gyro.getAngle();
+    		double distance2 = encoder.get();
     		if(!isAutonomous() || !isEnabled())
     			return;
-    		robot.drive(-.25, angle * Kp);
-    		if(distance < -500) {
-    			robot.drive(0, 0);
-    			break;
-    		}
+    			robot.drive(-.30, angle2 * Kp);
+    			if(distance2 < -3000) {
+    				robot.drive(0, 0);
+    				armmotor.set(Defines.ARM_OFF);
+    				leftarmwheel.set(Relay.Value.kOff);
+    		    	rightarmwheel.set(Relay.Value.kOff);
+    				break;
+    			}
     	}
     }
-        		
     public void operatorControl() {
-
     	while (isOperatorControl() && isEnabled()) {
-    	    double throttle = stick.getRawAxis(Defines.STICK_THROTTLE);
-       	    double leftaxis = xbox.getRawAxis(Defines.LEFT_AXIS);
-    	    double rightaxis = xbox.getRawAxis(Defines.RIGHT_AXIS);
+    		double throttle = stick.getRawAxis(Defines.STICK_THROTTLE);
+    		double leftaxis = xbox.getRawAxis(Defines.LEFT_AXIS);
+    		double rightaxis = xbox.getRawAxis(Defines.RIGHT_AXIS);
             bottomscrewlimit = limit.get(); 
             minarmlimit = limit3.get();
             maxarmlimit = limit4.get();
-    	    x = (-throttle + 1) / 2;
+    		x = (-throttle + 1) / 2;
     		
             robot.arcadeDrive(stick.getY() * x, -stick.getX() * x);
 
             //limitPressed2 = limit2.get();
             /*double angle = gyro.getAngle();
-    	      SmartDashboard.putNumber("angle", angle);
-    	      double distance = encoder.getDistance();
-    	      SmartDashboard.putNumber("distance", distance);*/
+    		SmartDashboard.putNumber("angle", angle);
+    		double distance = encoder.getDistance();
+    		SmartDashboard.putNumber("distance", distance);*/
             //System.out.println("limitPressed=" + limitPressed); //Read the RoboRIO log for some values.
             //System.out.println("limitPressed2=" + limitPressed2);
             //System.out.println("limitPressed3=" + limitPressed3);
@@ -204,9 +253,9 @@ public class Robot extends SampleRobot {
             	screwmotor2.set(Defines.SCREW_OFF);
             }
             
-            if(-rightaxis > 0.5 && maxarmlimit == true) { 
+            if(-rightaxis > 0.5 && maxarmlimit == true) { // && maxarmlimit == true
             	armmotor.set(Defines.ARM_SPEED);
-            } else if(rightaxis > 0.5 && minarmlimit == false) {
+            } else if(rightaxis > 0.5 && minarmlimit == true) { //&& minarmlimit == false
             	armmotor.set(-Defines.ARM_SPEED);
             } else {
             	armmotor.set(Defines.ARM_OFF);
@@ -229,4 +278,3 @@ public class Robot extends SampleRobot {
     	
     }
 }
-

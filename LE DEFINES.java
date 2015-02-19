203d73b3503729a88954cf43c56cf21ddc39a04b
curtis0gj <curtis.0gj@gmail.com>
2015-02-19 17:23:22 -0500

    Update LE DEFINES.java

--- LE DEFINES.java
+++ LE DEFINES.java
@@ -6,13 +6,13 @@ public class Defines {
 	public static final double ARM_SPEED = 0.2;
 	public static final double ARM_OFF = 0.0;
 	public static final double AUTO_TIMEBREAKS = 0.2;
-	
+
 	public static final int STICK_THROTTLE = 2;
 	public static final int LEFT_AXIS = 1;
 	public static final int RIGHT_AXIS = 5;
 	public static final int LEFT_TRIGGER = 5;
 	public static final int RIGHT_TRIGGER = 6;
-	
+
 	public enum AUTOS {
 		AUTO_TWOBLUE,
 		AUTO_TWORED,

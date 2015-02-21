ab9333c0a8bb31fb19a9e09fa0872867d2367bb1
curtis0gj <curtis.0gj@gmail.com>
2015-02-20 23:17:09 -0500

    Create oldleftandright.java

---
+++ oldleftandright.java
@@ -0,0 +1,24 @@
+	private static void turnRight(Robot r, double deg) {
+		while (true) {
+			double angle = r.gyro.getAngle();
+			if (!r.isAutonomous() || !r.isEnabled()) return;
+			r.robot.drive(-.40, -1);
+			if (angle > deg) {
+				reset(r);
+				return;
+			}
+			Timer.delay(0.02);
+		}
+	}
+	private static void turnLeft(Robot r, double deg) {
+		while (true) {
+			double angle = r.gyro.getAngle();
+			if (!r.isAutonomous() || !r.isEnabled()) return;
+			r.robot.drive(-.40, 1);
+			if (angle < deg) {
+				reset(r);
+				return;
+			}
+			Timer.delay(0.02);
+		}
+	}

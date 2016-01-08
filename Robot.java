24f02c45ff105ea3afabe2d3a39141d138f62415
curtis0gj <curtis.0gj@gmail.com>
2016-01-08 16:01:53 -0500

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,14 +1,15 @@
+
 package org.usfirst.frc.team5033.robot;
 
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Gyro;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Relay;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -25,18 +26,18 @@ public class Robot extends SampleRobot {
 	public Victor screwMotor2;
 	public Victor armMotor;
 	public DigitalInput limit;
-	public DigitalInput limit2;
-	public DigitalInput limit3;
+	/*public DigitalInput limit2;
+	public DigitalInput limit3;*/
 	public boolean bottomScrewLimit = false;
-	public boolean minArmLimit = false;
-	public boolean maxArmLimit = false;
+	/*public boolean minArmLimit = false;
+	public boolean maxArmLimit = false;*/
 	public double x = 0;
 	public SendableChooser autoChooser;
 	public Defines.Autos autoMethod;
 	public Auto auto;
 	
 	public Robot() {
-		chassis = new RobotDrive(0, 1);
+		chassis = new RobotDrive(0, 2);
 		stick = new Joystick(1);
 		xbox = new Joystick(0);
 		gyro = new Gyro(0);
@@ -44,21 +45,24 @@ public class Robot extends SampleRobot {
 		leftArmWheel = new Relay(1);
 		rightArmWheel = new Relay(2);
 		limit = new DigitalInput(4);
-		limit2 = new DigitalInput(6);
-		limit3 = new DigitalInput(7);
+		
+		/*limit2 = new DigitalInput(6);
+		limit3 = new DigitalInput(7);*/
+		
 		screwMotor1 = new Victor(4);
 		screwMotor2 = new Victor(5);
 		armMotor = new Victor(6);
+		
 		gyro.reset();
 		gyro.initGyro();
 		encoder.setDistancePerPulse(0.3193143);
 		encoder.getDistance();
+		
 		auto = new Auto(this);
 		autoChooser = new SendableChooser();
-		autoChooser.addDefault("Pick up two bins, blue side.", Defines.Autos.AUTO_GRAB_TWO_BINS_BLUE_SIDE);
-		autoChooser.addObject("Pick up two bins, red side.", Defines.Autos.AUTO_GRAB_TWO_BINS_RED_SIDE);
-		autoChooser.addObject("Pick up one bin, blue side.", Defines.Autos.AUTO_GRAB_ONE_BIN_BLUE_SIDE);
-		autoChooser.addObject("Pick up one bin, red side.", Defines.Autos.AUTO_GRAB_ONE_BIN_RED_SIDE);
+		autoChooser.addDefault("Do nothing.", Defines.Autos.AUTO_DO_NOTHING);
+		autoChooser.addObject("Lift one bin and stay.", Defines.Autos.AUTO_LIFT_BIN);
+		autoChooser.addObject("Pick up one bin.", Defines.Autos.AUTO_GRAB_ONE_BIN);
 		autoChooser.addObject("Move into the autonomous zone.", Defines.Autos.AUTO_MOVE_TO_ZONE);
 		SmartDashboard.putData("AUTONOMOUS MODES", autoChooser);
 	}
@@ -69,27 +73,31 @@ public class Robot extends SampleRobot {
 	public void operatorControl() {
 		while (isOperatorControl() && isEnabled()) {
 			double throttle = stick.getRawAxis(Defines.STICK_THROTTLE);
-			double leftAxis = xbox.getRawAxis(Defines.LEFT_AXIS);
+			
+			/*double leftAxis = xbox.getRawAxis(Defines.LEFT_AXIS);
 			double rightAxis = xbox.getRawAxis(Defines.RIGHT_AXIS);
-			bottomScrewLimit = limit.get();
 			minArmLimit = limit2.get();
-			maxArmLimit = limit3.get();
-			x = (-throttle + 1) / 2;
+			maxArmLimit = limit3.get();*/
+			
+			bottomScrewLimit = limit.get();
+			
+			x = (-throttle + 2) / 4;
 			chassis.arcadeDrive(stick.getY() * x, -stick.getX() * x);
-
-			if (-leftAxis > 0.5) {
+			
+			if (stick.getRawButton(3)) {
 				screwMotor1.set(Defines.SCREW_SPEED);
 				screwMotor2.set(Defines.SCREW_SPEED);
-			} else if (leftAxis > 0.5 && bottomScrewLimit == false) {
+			} else if (stick.getRawButton(2) && bottomScrewLimit == false) {
 				screwMotor1.set(-Defines.SCREW_SPEED);
 				screwMotor2.set(-Defines.SCREW_SPEED);
 			} else {
 				screwMotor1.set(Defines.SCREW_OFF);
-				screwMotor2.set(Defines.SCREW_OFF);
+				screwMotor2.set(Defines.SCREW_OFF);	
 			}
-			if (-rightAxis > 0.5 && maxArmLimit == true) {
+			
+			/*if (-rightAxis > 0.5 && maxArmLimit == false) {
 				armMotor.set(Defines.ARM_SPEED);
-			} else if (rightAxis > 0.5 && minArmLimit == true) {
+			} else if (rightAxis > 0.5 && minArmLimit == false) {
 				armMotor.set(-Defines.ARM_SPEED);
 			} else {
 				armMotor.set(Defines.ARM_OFF);
@@ -103,7 +111,8 @@ public class Robot extends SampleRobot {
 			} else {
 				leftArmWheel.set(Relay.Value.kOff);
 				rightArmWheel.set(Relay.Value.kOff);
-			}
+			}*/
+			
 			Timer.delay(0.02);
 		}
 	}

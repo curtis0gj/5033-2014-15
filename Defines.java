5df3cf705863899e8f7882c7fb19356e3e923eec
curtis0gj <curtis.0gj@gmail.com>
2016-01-08 16:02:35 -0500

    Update Defines.java

--- Defines.java
+++ Defines.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team5033.robot;
 
 public class Defines {
-	public static final double SCREW_SPEED = 0.7;
+	public static final double SCREW_SPEED = 0.5;
 	public static final double SCREW_OFF = 0.0;
 	public static final double ARM_SPEED = 0.2;
 	public static final double ARM_OFF = 0.0;
@@ -12,10 +12,9 @@ public class Defines {
 	public static final int RIGHT_TRIGGER = 6;
 
 	public enum Autos {
-		AUTO_GRAB_TWO_BINS_BLUE_SIDE,
-		AUTO_GRAB_TWO_BINS_RED_SIDE,
-		AUTO_GRAB_ONE_BIN_BLUE_SIDE,
-		AUTO_GRAB_ONE_BIN_RED_SIDE,
-		AUTO_MOVE_TO_ZONE
+		AUTO_GRAB_ONE_BIN,
+		AUTO_MOVE_TO_ZONE,
+		AUTO_LIFT_BIN,
+		AUTO_DO_NOTHING
 	}
 }
